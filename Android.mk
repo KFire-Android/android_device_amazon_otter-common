d80f96eb1b5916711c819f46b16787449a95831a
Hashcode <hashcode0f@gmail.com>
2013-01-29 11:12:21 -0800

    only build otter-common for otters

---
+++ Android.mk
@@ -0,0 +1,7 @@
+ifeq ($(findstring bowser, $(TARGET_BOOTLOADER_BOARD_NAME)),otter)
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(TARGET_SIMULATOR),true)
+include $(call first-makefiles-under,$(LOCAL_PATH))
+endif
+endif
