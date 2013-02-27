86718ee1c053b0ed3dc448ce283d131b63214d1c
Hashcode <hashcode0f@gmail.com>
2013-02-26 21:32:22 -0800

    ubuntu touch preview change

--- common.mk
+++ common.mk
@@ -71,8 +71,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=160 \
-    persist.lab126.chargeprotect=1 \
-    ro.sf.hwrotation=270
+    persist.lab126.chargeprotect=1
+
+#    ro.sf.hwrotation=270
 
 $(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/imgtec/amazon-common/device-partial-540.mk)
