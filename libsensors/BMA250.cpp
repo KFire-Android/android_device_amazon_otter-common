1ffbdea03c4adc8076cea2c6bae0034bba920bfa
Hashcode <hashcode0f@gmail.com>
2013-05-16 23:54:21 -0700

    sensors: correct for stock rotation (270)
    Change-Id: I5eb7ec533136b7f2720f18623c36080ceddc4cf9

--- libsensors/BMA250.cpp
+++ libsensors/BMA250.cpp
@@ -144,13 +144,13 @@ void BMA250Sensor::processEvent(int code, int value)
 */
     switch (code) {
         case EVENT_TYPE_ACCEL_X:
-            mPendingEvent.acceleration.x = value * CONVERT_A_X;
+            mPendingEvent.acceleration.x = value * CONVERT_A_Y;
             break;
         case EVENT_TYPE_ACCEL_Y:
-            mPendingEvent.acceleration.y = value * CONVERT_A_Y;
+            mPendingEvent.acceleration.y = value * CONVERT_A_X;
             break;
         case EVENT_TYPE_ACCEL_Z:
-            mPendingEvent.acceleration.z = value * CONVERT_A_Z;
+            mPendingEvent.acceleration.z = value * -CONVERT_A_Z;
             break;
     }
 }
