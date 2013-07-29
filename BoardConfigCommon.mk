36194a7e4b0f66beec63c72e664d69003ef1f7f8
Hashcode <hashcode0f@gmail.com>
2013-07-29 15:40:38 -0700

    revert back to TI wpa_supplicant_8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -73,8 +73,7 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_
 # Connectivity - Wi-Fi
 USES_TI_MAC80211 := true
 ifdef USES_TI_MAC80211
-#WPA_SUPPLICANT_VERSION           := VER_0_8_X_TI
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
+WPA_SUPPLICANT_VERSION           := VER_0_8_X_TI
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
 BOARD_HOSTAPD_DRIVER             := NL80211
@@ -85,7 +84,7 @@ WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
 WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
 WIFI_FIRMWARE_LOADER             := ""
 PRODUCT_WIRELESS_TOOLS           := true
-#COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
+COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
 endif
 
 # Custom DOMX
