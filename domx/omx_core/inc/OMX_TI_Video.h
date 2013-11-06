f26da5763bb3a1dd10beead94e40853a9d055bc7
Hashcode <hashcode0f@gmail.com>
2013-11-06 14:35:12 -0800

    domx: define conflict fix

--- domx/omx_core/inc/OMX_TI_Video.h
+++ domx/omx_core/inc/OMX_TI_Video.h
@@ -545,8 +545,8 @@ typedef enum OMX_TI_VIDEO_CODINGTYPE {
 		(OMX_VIDEO_CODINGTYPE) OMX_VIDEO_CodingVendorStartUnused +1,  /* VP6 */
 	OMX_VIDEO_CodingVP7, /* VP7 */
         OMX_TI_VIDEO_CodingSORENSONSPK, /* Sorenson spark*/
-        OMX_VIDEO_CodingSVC,    /**< H.264/SVC */
-	OMX_VIDEO_CodingVP8 /* VP8 */
+        OMX_VIDEO_CodingSVC    /**< H.264/SVC */
+//	OMX_VIDEO_CodingVP8 /* VP8 */
 }OMX_TI_VIDEO_CODINGTYPE;
 
 
