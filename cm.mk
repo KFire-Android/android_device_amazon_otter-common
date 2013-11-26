81d6f3f30a226001304af4cfdce9bae6669828fc
Hashcode <hashcode0f@gmail.com>
2013-11-25 22:58:46 -0800

    bootanim: fix pvr crash by using 480 version
    Change-Id: I80df44bcedd6c8155c1128d2f8c0537902803835

--- cm.mk
+++ cm.mk
@@ -15,7 +15,8 @@
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product-if-exists, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-TARGET_SCREEN_WIDTH := 600
+#TARGET_SCREEN_WIDTH := 600
+TARGET_SCREEN_WIDTH := 480
 TARGET_SCREEN_HEIGHT := 1024
 
 DEVICE_PACKAGE_OVERLAYS += device/amazon/otter-common/overlay/cm
