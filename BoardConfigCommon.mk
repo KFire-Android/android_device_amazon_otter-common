16758f6b7c8df97bb4c4d16fccf96a7680dc5467
Hashcode <hashcode0f@gmail.com>
2014-12-09 15:12:31 -0800

    wifi setup cleanup: removing framework module insertion (adding to init.*.rc)
    Change-Id: I98f3a429e970994fb08a029d9c4394518f0bcd17

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,17 +71,12 @@ USES_TI_MAC80211 := true
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
 BOARD_WLAN_DEVICE                := wl12xx_mac80211
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
-WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
-WIFI_FIRMWARE_LOADER             := ""
-BOARD_WIFI_SKIP_CAPABILITIES     := true
 ifdef USES_TI_MAC80211
 #WPA_SUPPLICANT_VERSION           := VER_0_8_X_TI
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wl12xx
 BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
-PRODUCT_WIRELESS_TOOLS           := true
 COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
 else
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
